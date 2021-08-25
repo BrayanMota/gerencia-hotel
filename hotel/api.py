efa03ab6616a3e9f5e36e15ec5a81285a215c87a
BrayanMota <brayanmota2001@hotmail.com>
2021-08-25 16:22:35 -0300

    API's de CRUD de quarto implementadas.

--- hotel/api.py
+++ hotel/api.py
@@ -2,6 +2,7 @@ from ninja import NinjaAPI, Schema, Path
 from .schemas import QuartoSchema
 from .models import Quarto
 from django.shortcuts import get_object_or_404
+from typing import List
 
 api = NinjaAPI()
 
@@ -9,6 +10,31 @@ api = NinjaAPI()
 def hello(request):
     return "Hello world"
 
-@api.post('/lista_quartos')
-def lista_quartos(request, quartos: Quarto):
+@api.post('/cadastra_quarto')
+def cadastra_quarto(request, quartos: QuartoSchema):
+  quarto = Quarto.objects.create(**quartos.dict())
+  return {"quarto_id": quarto.quarto_id}
+
+@api.put("/edita_quarto/{quarto_id}")
+def edita_quarto(request, quartos: QuartoSchema, quarto_id: int):
+    quarto = get_object_or_404(Quarto, quarto_id=quarto_id)
+    for attr, value in quartos.dict().items():
+        setattr(quarto, attr, value)
+    quarto.save()
+    return {"editado": True}
+
+@api.get('/lista_quartos', response=List[QuartoSchema])
+def lista_quartos(request):
+  quartos = Quarto.objects.order_by('quarto_id')
   return quartos
+
+@api.get('/detalhe_quarto/{quarto_id}', response=QuartoSchema)
+def detalhe_quarto(request, quarto_id: int):
+  quarto = get_object_or_404(Quarto, quarto_id=quarto_id)
+  return quarto
+
+@api.delete('/deleta_quarto/{quarto_id}')
+def deleta_quarto(request, quarto_id: int):
+  quarto = get_object_or_404(Quarto, quarto_id=quarto_id)
+  quarto.delete()
+  return {'deletado': True}
--- hotel/schemas.py
+++ hotel/schemas.py
@@ -1,8 +1,15 @@
 from ninja import Schema
 
+from hotel.models import Quarto
+
 class QuartoSchema(Schema):
+  quarto_id: int
   nome: str
   numero: int
   preco: float
   camas: int
   frigobar: bool
+  
+  # class Meta:
+  #   model = Quarto
+  #   fields = '__all__'
